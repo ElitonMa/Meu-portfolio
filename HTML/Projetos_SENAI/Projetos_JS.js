0390776612ab4277552eec81b6031f4bf6c903d0
ELITON MASUREK <eliton_masurek@estudante.sesisenai.org.br>
2025-05-30 16:15:42 -0300

    javascript funcionando

--- HTML/Projetos_SENAI/Projetos_JS.js
+++ HTML/Projetos_SENAI/Projetos_JS.js
@@ -1,35 +1,41 @@
 
-
 function calcularIMC () {
-    let peso = document.getElementById("peso").value
-    let altura = document.getElementById("altura").value
-    let IMC = peso / (altura*altura)
+    const textoSaude = document.getElementById("grau-saude").textContent;
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+    // Caso digite altura ou peso invalido
+    if (altura <= 0 || peso <= 0) {
+        return;
+    }
 
-        document.getElementById("sobrepeso").hidden = true;  
-        document.getElementById("abaixo").hidden = true; 
-        document.getElementById("normal").hidden = true; 
-        document.getElementById("obesidadeGrau1").hidden = true; 
-        document.getElementById("obesidadeGrau2").hidden = true;
-        document.getElementById("obesidadeGrau3").hidden = true; 
+    else {
+        let classificacao = "";
 
-    if (IMC < 18.5) {
-        document.getElementById("sobrepeso").hidden = false;  
+            // Formula do IMC
+            let IMC = peso / (altura*altura);
+            
+            // Verificacoes
+            if (IMC < 18.5) {
+                classificacao = "Você esta abaixo do normal";
+            }
+            else if (IMC >= 18.6 && IMC <= 24.9) {
+                classificacao = "Você esta dentro do normal";
+            }
+            else if (IMC >= 25 && IMC <= 29.9) {
+                classificacao = "Você esta sobrepeso";
+            }
+            else if (IMC >= 30 && IMC <= 34.9) {
+                classificacao = "Você esta com grau de obesidade I";
+            }
+            else if (IMC >= 35 && IMC <= 39.9) {
+                classificacao = "Você esta com grau de obesidade II";
+            }
+            else if (IMC >= 40) {
+                classificacao = "Você esta com grau de obesidade III";
+            }
+
+            document.getElementById("grau-saude").textContent = `${classificacao}`;
+            document.getElementById("imc").value = IMC.toFixed(1);
     }
-    else if (IMC >= 18.6 && IMC <= 24.9) {
-        document.getElementById("abaixo").hidden = false; 
-    }
-    else if (IMC >= 25 && IMC <= 29.9) {
-        document.getElementById("normal").hidden = false;  
-    }
-    else if (IMC >= 30 && IMC <= 34.9) {
-        document.getElementById("obesidadeGrau1").hidden = false; 
-    }
-    else if (IMC >= 35 && IMC <= 39.9) {
-        document.getElementById("obesidadeGrau2").hidden = false;       
-    }
-    else if (IMC >= 40) {
-        document.getElementById("obesidadeGrau3").hidden = false;
-    }
-    document.getElementById("imc").value = IMC.toFixed(1)
 }
     
