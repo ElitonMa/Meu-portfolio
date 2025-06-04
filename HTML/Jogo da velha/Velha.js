818b24e2a312ba69a14e37fa8611ef63d74b26db
ELITON MASUREK <eliton_masurek@estudante.sesisenai.org.br>
2025-06-04 17:19:41 -0300

    velha css js htm

--- HTML/Jogo da velha/Velha.js
+++ HTML/Jogo da velha/Velha.js
@@ -1,4 +1,28 @@
+let rodadaAtual = 0;
+let jogadorAtual = "";
+function incrementarRodada () {
+    rodadaAtual++;
+    return rodadaAtual;
+}
 
-function anularValor(id) {
-    document.getElementById(`${id}`).textContent = ""; 
+function verificarJogadorAtual () {
+    if (rodadaAtual % 2 === 0){
+        jogadorAtual = "X";
+    }
+    else {
+        jogadorAtual = "O";
+    }
+    return jogadorAtual;
+}
+
+
+function mostrarJogadorAtual (idMostrar) {
+    console.log(rodadaAtual);
+    console.log(jogadorAtual);
+    document.getElementById(`${idMostrar}`).textContent = jogadorAtual;
+}
+
+function fazerJogada (idReal) {
+    incrementarRodada();    
+    document.getElementsByClassName(`${idReal}`).textContent = jogadorAtual;
 }
