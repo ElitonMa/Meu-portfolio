53d0f34adb83282b0fde65f8e0b937c40b558ecf
ELITON MASUREK <eliton_masurek@estudante.sesisenai.org.br>
2025-06-05 15:24:07 -0300

    js meio-funcional

--- HTML/Jogo da velha/Velha.js
+++ HTML/Jogo da velha/Velha.js
@@ -1,28 +1,56 @@
 let rodadaAtual = 0;
-let jogadorAtual = "";
+let jaJogou = [false,false,false,false,false,false,false,false,false];
+let jogadorAtual;
+
 function incrementarRodada () {
     rodadaAtual++;
-    return rodadaAtual;
 }
 
 function verificarJogadorAtual () {
-    if (rodadaAtual % 2 === 0){
-        jogadorAtual = "X";
+
+    if (rodadaAtual % 2 == 0) {
+        jogadorAtual = 'X';
     }
     else {
-        jogadorAtual = "O";
+        jogadorAtual = 'O';
     }
     return jogadorAtual;
 }
 
+function mostrarJogadorAtual (temporario, idReal) {
 
-function mostrarJogadorAtual (idMostrar) {
-    console.log(rodadaAtual);
-    console.log(jogadorAtual);
-    document.getElementById(`${idMostrar}`).textContent = jogadorAtual;
+    let jogadaTemporaria = document.querySelector(`${temporario}`);
+    let jogada = document.querySelector(`${idReal}`);
+    console.log(jogada.textContent);
+    if (jogada === "O" || jogada === "X") {
+        return;
+    }
+
+    else {
+
+    jogadaTemporaria.style.opacity = "0.8"
+    jogadaTemporaria.textContent = verificarJogadorAtual();
+    jogadaTemporaria.onmouseout = function() {naoMostrar()};
+
+        function naoMostrar() {
+            jogadaTemporaria.textContent = ""; 
+            jogadaTemporaria.style.opacity = "1"
+        };
+    
+    }
 }
 
-function fazerJogada (idReal) {
-    incrementarRodada();    
-    document.getElementsByClassName(`${idReal}`).textContent = jogadorAtual;
+function fazerJogada (temporario, idReal, quadrado) { 
+    
+    document.querySelector(`${temporario}`).textContent = "";
+    let jogada = document.querySelector(`${idReal}`); 
+
+    if (jogada.textContent === "O" || jogada.textContent === "X") {
+        return;
+    }
+    else {
+        jogada.textContent = verificarJogadorAtual();
+        document.querySelector(`${quadrado}`).textContent = jogada;
+        incrementarRodada(); 
+    }
 }
