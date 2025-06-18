03f1445c8e18abe91df2738f4c07d53ac6def656
ELITON MASUREK <eliton_masurek@estudante.sesisenai.org.br>
2025-06-18 17:24:37 -0300

    adicionado imagens

--- HTML/Quiz/Quiz.js
+++ HTML/Quiz/Quiz.js
@@ -1,4 +1,4 @@
-let contextoQuestoesC = ['Qual é a função de ponteiros em C','Qual é a funcionalidade de fgets em C','Para que serve structs em C','Qual é a definição de vetores?','Qual é o simbolo correto para atribuir um valor a outro?'];
+let contextoQuestoesC = ['Qual é a função de ponteiros em C?','Qual é a funcionalidade de fgets em C?','Para que serve structs em C?','Qual é a definição de vetores?','Qual é o simbolo correto para atribuir um valor a outro?'];
 let alternativasQuestoesC = [
  ['Armazenar o endereço de memória de outra variável','Verificar o endereço de memória de outra variável','Receber os valores de outra variável','Verificar o tipo de memória de outra variável'],
  ['Usado para ler um nome','Usado para ler uma lista de numeros','Usado para ler uma linha de texto','Usado para ler um vetor'],
@@ -16,11 +16,14 @@ function aparecerQuiz(nomeQuiz) {
 
 let rodadaAtual = 0;
 
+let imagensC = ['https://gabrielbueno072.github.io/rea-aed/img/pon/imagem24.PNG','https://miro.medium.com/v2/resize:fit:1400/1*QQPiemm-CdnfEgEt4-h2aw.png','https://miro.medium.com/v2/resize:fit:1070/1*Zsiup68sgQjC4loxUhUzEg.png','https://i.ytimg.com/vi/a_CCS47OTck/maxresdefault.jpg','https://linguagemc.com.br/wp-content/uploads/2012/03/OperadoresAritmeticos.jpg'];
+
 function mudarPerguntaC() {
 
     if (!verificarSeExcedeuLimite()) {
 
         document.querySelector("#pergunta-C").textContent = contextoQuestoesC[rodadaAtual];
+        document.getElementById("imagem-C").src = imagensC[rodadaAtual]; 
         document.querySelector("#pergunta-1-C").textContent = alternativasQuestoesC[rodadaAtual][0];
         document.querySelector("#pergunta-2-C").textContent = alternativasQuestoesC[rodadaAtual][1];
         document.querySelector("#pergunta-3-C").textContent = alternativasQuestoesC[rodadaAtual][2];
