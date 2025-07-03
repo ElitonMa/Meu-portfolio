58472cd3dde7d1b54e430ed2788b03f463dbd004
ElitonMa <eliton_masurek@estudante.sesisenai.org.br>
2025-07-03 20:25:33 +0000

    adicionado botão para adicionar novos elementos

---
+++ HTML/Projetos_pessoais/Lista de tarefas/lista_tarefas_JS.js
@@ -0,0 +1,28 @@
+    const conteudo = document.createElement("div");
+    const label = document.createElement("label");
+
+function adicionarElemento() {
+    const elemento = document.getElementById("fundo-lista");
+    const lista = document.createElement("div");
+    const br = document.createElement("br");
+
+    label.innerHTML = "Texto exemplo";
+    conteudo.innerHTML = "Exemplo de conteudo";
+
+    lista.className = "item-lista";
+    conteudo.id = "conteudo";
+
+    elemento.appendChild(lista);
+    lista.appendChild(label);
+    lista.appendChild(br);
+    lista.appendChild(conteudo);
+}
+
+function tornarMudavel() {
+    let contador = 0;
+
+    if (contador = 0) {
+        contador++;
+        conteudo.contentEditable = true;
+    }
+}
