07cd4ada09bf6aeb21dfd658743ccab32a024641
ElitonMa <eliton_masurek@estudante.sesisenai.org.br>
2025-07-03 18:41:05 +0000

    finalizado calculadora de imc

--- HTML/Projetos_SENAI/Calculadora_IMC/Calculadora_JS.js
+++ HTML/Projetos_SENAI/Calculadora_IMC/Calculadora_JS.js
@@ -1,6 +1,6 @@
 
 function calcularIMC () {
-    const textoSaude = document.getElementById("grau-saude").textContent;
+    const textoSaude = document.getElementById("grau-saude");
     let peso = document.getElementById("peso").value;
     let altura = document.getElementById("altura").value;
     // Caso digite altura ou peso invalido
@@ -16,26 +16,32 @@ function calcularIMC () {
             
             // Verificacoes
             if (IMC < 18.5) {
-                classificacao = "Você esta abaixo do normal";
+                classificacao = "Você está abaixo do normal";
+                textoSaude.style.color = "#7cf797";
             }
             else if (IMC >= 18.6 && IMC <= 24.9) {
-                classificacao = "Você esta dentro do normal";
+                classificacao = "Você está dentro do normal";
+                textoSaude.style.color = "#37f360";
             }
             else if (IMC >= 25 && IMC <= 29.9) {
-                classificacao = "Você esta sobrepeso";
+                classificacao = "Você está sobrepeso";
+                textoSaude.style.color = "#eb7575";
             }
             else if (IMC >= 30 && IMC <= 34.9) {
-                classificacao = "Você esta com grau de obesidade I";
+                classificacao = "Você está com grau de obesidade I";
+                textoSaude.style.color = "#db3434";
             }
             else if (IMC >= 35 && IMC <= 39.9) {
-                classificacao = "Você esta com grau de obesidade II";
+                classificacao = "Você está com grau de obesidade II";
+                textoSaude.style.color = "#db3434";
             }
             else if (IMC >= 40) {
-                classificacao = "Você esta com grau de obesidade III";
+                classificacao = "Você está com grau de obesidade III";
+                textoSaude.style.color = "#db0e0e       ";
             }
 
-            document.getElementById("grau-saude").textContent = `${classificacao}`;
-            document.getElementById("imc").value = IMC.toFixed(1);
+            textoSaude.textContent = `${classificacao}`;
+            document.getElementById("imc").textContent =  'IMC = ' + IMC.toFixed(1);
     }
 }
     
