c397829b27c442b66dbf23a337b0c03c41a3198f
ELITON MASUREK <eliton_masurek@estudante.sesisenai.org.br>
2025-07-04 14:45:06 -0300

    corrigido para que todas as listas possam ser editadas

--- HTML/Projetos_pessoais/Lista de tarefas/lista_tarefas_JS.js
+++ HTML/Projetos_pessoais/Lista de tarefas/lista_tarefas_JS.js
@@ -1,28 +1,55 @@
-    const conteudo = document.createElement("div");
-    const label = document.createElement("label");
+    let quantidadeDeListas = 1;
 
 function adicionarElemento() {
+    const conteudo = document.createElement("div");
+    const label = document.createElement("label");
     const elemento = document.getElementById("fundo-lista");
     const lista = document.createElement("div");
     const br = document.createElement("br");
 
-    label.innerHTML = "Texto exemplo";
-    conteudo.innerHTML = "Exemplo de conteudo";
-
     lista.className = "item-lista";
-    conteudo.id = "conteudo";
+
+    label.innerHTML = "Texto exemplo";
+    label.id = "label" + `${quantidadeDeListas}`;
+
+    conteudo.innerHTML = "Exemplo de conteudo";
+    conteudo.id = "conteudo" + `${quantidadeDeListas}`;
+
+    quantidadeDeListas++;
 
     elemento.appendChild(lista);
     lista.appendChild(label);
     lista.appendChild(br);
-    lista.appendChild(conteudo);
+    lista.appendChild(conteudo);        
 }
 
-function tornarMudavel() {
-    let contador = 0;
+let pressionouEditar = false;
 
-    if (contador = 0) {
-        contador++;
-        conteudo.contentEditable = true;
+function tornarEditavel() {
+    let lapisDesativado = document.getElementById("mudar-texto-desativado");
+    let lapisAtivado = document.getElementById("mudar-texto-ativado");
+
+    for (let i = 0; i < quantidadeDeListas; i++ ) {
+
+        let elementosLabel = document.getElementById("label" + `${i}`);
+        let elementosConteudo = document.getElementById("conteudo" + `${i}`);
+
+        if (i === quantidadeDeListas-1) {
+            
+            pressionouEditar = !pressionouEditar;
+        }
+        if (pressionouEditar === false ) {
+            lapisDesativado.style.display = "none";
+            lapisAtivado.style.display = "inline-block";
+            elementosConteudo.contentEditable = "true";
+            elementosLabel.contentEditable = "true";
+        }
+        else if (pressionouEditar === true) {
+            lapisDesativado.style.display = "inline-block";
+            lapisAtivado.style.display = "none";
+            elementosConteudo.contentEditable = "false";
+            elementosLabel.contentEditable = "false";
+        }
     }
+
 }
