83ef70899b371cc9b5718b1ee80c570cf98368e4
ElitonMa <eliton_masurek@estudante.sesisenai.org.br>
2025-07-10 19:09:58 +0000

    adicionado site de login

---
+++ HTML/Projetos_SENAI/Login/login.js
@@ -0,0 +1,18 @@
+
+const senhaInvisivel = document.getElementById("mostrar-senha");
+const inputSenha = document.getElementById("senha");
+let clicouOlho = false;
+
+senhaInvisivel.addEventListener("click", function () {
+
+    if (!clicouOlho) {
+        senhaInvisivel.setAttribute("icon", "mdi:eye-off");
+        inputSenha.setAttribute("type", "text");
+        clicouOlho = true;
+    }
+    else if (clicouOlho) {
+        inputSenha.setAttribute("type", "password");
+        senhaInvisivel.setAttribute("icon", "mdi:eye");
+        clicouOlho = false;
+    }
+})
